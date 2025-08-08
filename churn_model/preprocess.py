2203baa12bb1b1153cc455c029df9e4e83834319
Harshit254-p <harshitpandya254@gmail.com>
2025-08-08 10:16:48 +0530

    Update preprocess.py

--- churn_model/preprocess.py
+++ churn_model/preprocess.py
@@ -6,9 +6,10 @@ def preprocess_input(data):
     """
     df = pd.DataFrame([data])
 
-    # Ensure proper types
+    
     df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce")
     df["gender"] = df["gender"].map({"Female": 0, "Male": 1})
     df["SeniorCitizen"] = df["SeniorCitizen"].apply(lambda x: 1 if x in [1, "1", "Yes", "yes", True] else 0)
 
     return df[["gender", "SeniorCitizen", "MonthlyCharges", "TotalCharges"]]
+
